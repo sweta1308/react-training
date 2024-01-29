842a84f76086ff04b4219215d460a80bd2eba236
Sweta Agarwalla <swetaagarwalla@Swetas-MacBook-Air.local>
2024-01-29 19:48:22 +0530

    add components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { Name } from "./components/Name";
+import { ProductList } from "./components/ProductList";
 
 function App() {
+  const [name, setName] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const handleBackClick = () => setDisplayName("");
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      {displayName ? (
+        <div>
+          <h1>Welcome, {displayName}!</h1>
+          <button onClick={handleBackClick}>Go back</button>
+        </div>
+      ) : (
+        <Name name={name} setName={setName} setDisplayName={setDisplayName} />
+      )}
+      <hr />
+      <ProductList />
     </div>
   );
 }
